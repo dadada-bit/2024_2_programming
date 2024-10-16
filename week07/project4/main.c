306de5a7c36d2adb4c7127322e80a081f0d65ec6
dadada-bit <minj4721@gmail.com>
2024-10-16 14:27:34 +0900

    week07_project4

---
+++ week07/project4/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include <stdio.h>
+
+// 함수 선언
+int get_integer(const char *message);
+int factorial(int n);
+int combination(int n, int r);
+
+int main(void) {
+
+    int n, r;
+
+    n = get_integer("Enter the value of n: ");
+    r = get_integer("Enter the value of r: ");
+
+    int result = combination(n, r);
+
+    printf("The result of C(%d, %d) is %d\n", n, r, result);
+
+    return 0;
+}
+
+int combination(int n, int r) {
+    return factorial(n) / (factorial(n - r) * factorial(r));
+}
+
+int factorial(int n) {
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+int get_integer(const char *message) {
+    int value;
+    printf("%s", message);
+    scanf("%d", &value);
+    return value;
+}
