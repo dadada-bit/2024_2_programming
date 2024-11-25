6b619ab36a87a44a4a63deb85b7f29c15362e1fb
dadada-bit <minj4721@gmail.com>
2024-11-26 00:40:52 +0900

    week12_project7

---
+++ week12/project7/main.c
@@ -0,0 +1,41 @@
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+char *proverb="All that glisters is not gold.";
+void setPointer(char *q) 
+{
+	q = proverb;
+}
+
+int main(void)
+{
+	char *p ="zzz";
+	setPointer(p);
+	printf("%s \n",p);
+	
+	return 0;
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char *proverb="All that glisters is not gold.";
+void setPointer(char **q) 
+{
+	*q = proverb;
+}
+
+int main(void)
+{
+	char *p = "zzz";
+	setPointer(&p);
+	printf("%s\n", p);
+	
+	return 0;
+}
